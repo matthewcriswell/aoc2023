d643b717051d8edcbf399c09cf107fd2d96bd9d6
Matt Criswell <llewsircttam@gmail.com>
2023-12-05 17:37:22 -0600

    day2 part2 complete

---
+++ 2/2.py
@@ -0,0 +1,33 @@
+# with open('test.txt', 'r') as infile:
+with open("input.txt", "r") as infile:
+    lines = [line.strip() for line in infile]
+
+
+counters = list()
+game_num = 1
+for game in [line.split(";") for line in [line.split(":")[1].strip() for line in lines]]:
+    play_dicts = list()
+    for play in game:
+        lst = play.strip().split()
+        play_result_dict = {}
+        for i in range(0, len(lst), 2):
+            key = lst[i + 1].rstrip(",")  # Remove any trailing commas
+            value = int(lst[i])  # Convert string to integer
+            play_result_dict[key] = value
+        play_dicts.append(play_result_dict)
+        result_dict = dict()
+        for play_dict in play_dicts:
+            for item in play_dict:
+                if item in result_dict:
+                    if play_dict[item] >= result_dict[item]:
+                        result_dict[item] = play_dict[item]
+                else:
+                    result_dict[item] = play_dict[item]
+    counters.append([game_num, result_dict])
+    game_num += 1
+
+final_result = 0
+for game in counters:
+    final_result += game[1]["red"] * game[1]["green"] * game[1]["blue"]
+
+print(final_result)
